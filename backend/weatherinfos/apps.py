82d5bfcfc728fea850b5dd474ea32cccbc81267c
vvchldmsdn <vvchldmsdn@gmail.com>
2022-06-18 02:34:25 +0900

    0618 0234

---
+++ backend/weatherinfos/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WeatherinfosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'weatherinfos'
---
+++ backend/weatherinfos/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Weather(models.Model):
+    temp = models.FloatField()
+    feels_like = models.FloatField()
+    humidity = models.FloatField()
+    wind_speed = models.FloatField()
+    wind_deg = models.FloatField()
+    cloud = models.FloatField()
